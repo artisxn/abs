fb7cd55910e732217040c877b8ceb4e6717aa073
kawax <kawaxbiz@gmail.com>
2017-09-09 15:56:45 +0900

    travis

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'name' => 'Amazon Product Advertising API Demo',
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
