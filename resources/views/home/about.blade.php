d22804a7f40077a47945a615b9c7d6a7d1e19b6a
kawax <kawaxbiz@gmail.com>
2017-09-16 23:49:50 +0900

    about

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,5 +1,7 @@
 <div class="uk-card uk-card-default uk-card-body">
   <h2 class="uk-card-title">このサイトについて</h2>
-  <p>APIのデモサイトとして検索機能だけ使えるようにしてましたが現在は徐々に機能追加中。</p>
+  <p>
+    APIのデモサイトとして検索機能だけ使えるようにしてましたが現在は徐々に機能追加中。追加済はAmazonアカウントでログインとウォッチリスト。URLも変更。
+  </p>
 
 </div>
