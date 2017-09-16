8e233d84a31a31015723869fb2b65adf40e0949a
kawax <kawaxbiz@gmail.com>
2017-09-16 21:29:45 +0900

    ga

--- resources/views/layouts/analytics.blade.php
+++ resources/views/layouts/analytics.blade.php
@@ -1,5 +1,5 @@
 <!-- Global Site Tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-188090-2"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('amazon.analytics') }}"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments)};
