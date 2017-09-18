a1d276332d9fce7b758bd579e494037033ab8131
kawax <kawaxbiz@gmail.com>
2017-09-18 20:30:02 +0900

    ga

--- config/amazon.php
+++ config/amazon.php
@@ -3,14 +3,14 @@ return [
     'analytics' => env('GOOGLE_ANALYTICS', ''),
 
     'redirect_from' => env('REDIRECT_FROM'),
-    'redirect_to' => env('REDIRECT_TO'),
+    'redirect_to'   => env('REDIRECT_TO'),
 
     'form' => [
         'All'         => 'すべて',
         'Books'       => '本',
         'Music'       => 'ミュージック',
         'Electronics' => '家電&カメラ',
-        'Appliances'  => '大型家電	',
+        'Appliances'  => '大型家電',
         'VideoGames'  => 'TVゲーム',
 
         'Apparel' => '服＆ファッション小物',
