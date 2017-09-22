b74f9c1bc85338a506ccd9531f899e61ea20f5d9
kawax <kawaxbiz@gmail.com>
2017-09-22 14:06:28 +0900

    test

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -4,5 +4,5 @@
     Amazon Product Advertising APIのデモサイトとして検索機能だけ使えるようにしてましたが現在は徐々に機能追加中。追加済はAmazonアカウントでログインとウォッチリスト。10-15年くらい使ってたURLも変更。
   </p>
   <p>今後の予定は履歴データが増えたらグラフ化。</p>
-  <p>ASINカウント : {{ $items_count }} / 履歴カウント : {{ $histories_count }}</p>
+  <p>ASINカウント : {{ $items_count or '0' }} / 履歴カウント : {{ $histories_count or '0' }}</p>
 </div>
