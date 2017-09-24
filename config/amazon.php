001d6e1643345c8282014be082dfb224822c23b6
kawax <kawaxbiz@gmail.com>
2017-09-25 01:54:30 +0900

    search form

--- config/amazon.php
+++ config/amazon.php
@@ -6,28 +6,25 @@ return [
     'redirect_to'   => env('REDIRECT_TO'),
 
     'form' => [
-        'All'         => 'すべて',
-        'Books'       => '本',
-        'Music'       => 'ミュージック',
-        'Electronics' => '家電&カメラ',
-        'Appliances'  => '大型家電',
-        'VideoGames'  => 'TVゲーム',
-
-        'Apparel' => '服＆ファッション小物',
-
-        'Toys' => 'おもちゃ',
-
-        'Software'   => 'PCソフト',
-        'PCHardware' => 'パソコン・周辺機器',
-
+        'All'                => 'すべて',
+        'Books'              => '本',
+        'Music'              => 'ミュージック',
+        'DVD'                => 'DVD',
+        'Electronics'        => '家電&カメラ',
+        'Appliances'         => '大型家電',
+        'VideoGames'         => 'TVゲーム',
+        'Apparel'            => '服＆ファッション小物',
+        'Toys'               => 'おもちゃ',
+        'Software'           => 'PCソフト',
+        'PCHardware'         => 'パソコン・周辺機器',
+        'OfficeProducts'     => '文房具・オフィス用品',
         'HealthPersonalCare' => 'ヘルス&ビューティー',
         'Beauty'             => 'コスメ',
-
-        'Shoes'   => 'シューズ＆バッグ',
-        'Jewelry' => 'ジュエリー',
-
-        'Baby' => 'ベビー&マタニティ',
-
-        'Hobbies' => 'ホビー',
+        'SportingGoods'      => 'スポーツ&アウトドア',
+        'Shoes'              => 'シューズ＆バッグ',
+        'Jewelry'            => 'ジュエリー',
+        'Baby'               => 'ベビー&マタニティ',
+        'Hobbies'            => 'ホビー',
+        'Automotive'         => 'カー・バイク用品',
     ],
 ];
