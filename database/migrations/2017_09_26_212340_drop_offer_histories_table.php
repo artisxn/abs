6db7c1c15631886a1379a68990dbd9519f4ef4c3
kawax <kawaxbiz@gmail.com>
2017-09-26 21:38:52 +0900

    drop offer

---
+++ database/migrations/2017_09_26_212340_drop_offer_histories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropOfferHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('histories', function (Blueprint $table) {
+            $table->dropColumn('offer');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('histories', function (Blueprint $table) {
+            $table->json('offer')->nullable();
+        });
+    }
+}
