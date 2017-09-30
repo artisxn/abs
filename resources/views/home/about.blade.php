489d171314f399cfd249cecec00357b7673d4184
kawax <kawaxbiz@gmail.com>
2017-09-30 22:24:47 +0900

    about

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -3,6 +3,17 @@
   <p>
     Amazon Product Advertising APIのデモサイトとして検索機能だけ使えるようにしてましたが現在は徐々に機能追加中。10-15年くらい使ってたURLも変更。
   </p>
-  <p>今後の予定は履歴データが増えたらグラフ化。</p>
 
+  <h4>今後の予定</h4>
+  <ul class="uk-list uk-list-bullet">
+    <li>履歴データが増えたらグラフ化</li>
+    <li>価格が変動したアイテムを通知</li>
+  </ul>
+
+  <h4>追加済み</h4>
+  <ul class="uk-list uk-list-bullet">
+    <li>Amazonアカウントでログイン</li>
+    <li>ウォッチリスト</li>
+    <li>CSVでダウンロード</li>
+  </ul>
 </div>
