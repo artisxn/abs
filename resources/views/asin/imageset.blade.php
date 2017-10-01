9d8690720f11f21dfc653fa9c22cd4b5f0774096
kawax <kawaxbiz@gmail.com>
2017-10-02 01:28:29 +0900

    image sets

--- resources/views/asin/imageset.blade.php
+++ resources/views/asin/imageset.blade.php
@@ -3,28 +3,32 @@
   if(array_has($image_sets, '@attributes')){
     $image_sets =[$image_sets];
   }
+
+  if(array_has($image_sets, '0.HiResImage.URL')){
+    $image_size = 'HiResImage';
+  } else {
+    $image_size = 'LargeImage';
+  }
+
+  $image_urls = array_pluck($image_sets, $image_size . '.URL');
 @endphp
 
-@unless(empty($image_sets))
+@unless(empty($image_urls))
   <div class="uk-card-body">
 
     <h4 class="uk-heading-line"><span>画像</span></h4>
 
     <div uk-lightbox>
 
-      @foreach($image_sets as $image_set)
-        @unless(empty(array_get($image_set, 'HiResImage.URL')))
-
-          <a href="{{ array_get($image_set, 'HiResImage.URL') }}">
-            <div class="uk-cover-container uk-height-medium">
-              <img src="{{ array_get($image_set, 'HiResImage.URL') }}"
-                   alt=""
-                   itemprop="image"
-                   uk-cover>
-            </div>
-          </a>
-
-        @endunless
+      @foreach($image_urls as $image_url)
+        <a href="{{ $image_url }}">
+          <div class="uk-cover-container uk-height-medium">
+            <img src="{{ $image_url }}"
+                 alt=""
+                 itemprop="image"
+                 uk-cover>
+          </div>
+        </a>
       @endforeach
 
     </div>
