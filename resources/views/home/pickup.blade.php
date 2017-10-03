73a65be925375d9a6cec46daeb49c05424946c9a
kawax <kawaxbiz@gmail.com>
2017-10-03 09:58:05 +0900

    pu

--- resources/views/home/pickup.blade.php
+++ resources/views/home/pickup.blade.php
@@ -5,9 +5,13 @@
 
   <div class="uk-section uk-section-small uk-section-muted uk-padding">
     <div class="uk-container">
-      @foreach($pickup_posts as $post)
-        {!! $post->body !!}
-      @endforeach
+      <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+        @foreach($pickup_posts as $post)
+          <div>
+            {!! $post->body !!}
+          </div>
+        @endforeach
+      </div>
     </div>
   </div>
 
