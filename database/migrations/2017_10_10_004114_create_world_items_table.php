79c4f38a64a5aabc948987dc48163be81945912e
kawax <kawaxbiz@gmail.com>
2017-10-10 18:15:02 +0900

    [WIP]world

---
+++ database/migrations/2017_10_10_004114_create_world_items_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorldItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('world_items', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->string('asin', 10)->index();
+            $table->string('ean', 13)->index()->nullable();
+            $table->string('country', 2)->index();
+
+            $table->string('title')->nullable();
+            $table->unsignedInteger('rank')->nullable();
+
+            $table->unsignedInteger('lowest_new_price')->nullable();
+
+            $table->unsignedInteger('lowest_used_price')->nullable();
+
+            $table->unsignedInteger('total_new')->nullable();
+
+            $table->unsignedInteger('total_used')->nullable();
+
+            $table->string('availability')->nullable();
+
+            $table->text('editorial_review')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('world_items');
+    }
+}
