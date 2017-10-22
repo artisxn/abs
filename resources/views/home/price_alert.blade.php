45c63cace1f56a29f84b436cb78d0211756995b0
kawax <kawaxbiz@gmail.com>
2017-10-22 17:09:17 +0900

    price

--- resources/views/home/price_alert.blade.php
+++ resources/views/home/price_alert.blade.php
@@ -15,15 +15,19 @@
             <td>
               @if($post->category_id === 2)
                 <span class="uk-icon-button uk-background-default uk-text-success" uk-icon="icon: arrow-up"></span>
-              @endif
-
-              @if($post->category_id === 3)
+              @elseif($post->category_id === 3)
                 <span class="uk-icon-button uk-background-default uk-text-danger" uk-icon="icon: arrow-down"></span>
               @endif
 
             </td>
             <td><a href="{{ route('asin', $post->excerpt) }}">{{ str_limit($post->title, 200) }}</a></td>
-            <td class="uk-text-nowrap">{{ $post->body }}</td>
+            <td class="uk-text-nowrap">
+              @if($post->category_id === 2)
+                <span class="uk-text-success">{{ $post->body }}</span>
+              @elseif($post->category_id === 3)
+                <span class="uk-text-danger">{{ $post->body }}</span>
+              @endif
+            </td>
             <td class="uk-text-nowrap">{{ $post->updated_at }}</td>
           </tr>
         @endforeach
