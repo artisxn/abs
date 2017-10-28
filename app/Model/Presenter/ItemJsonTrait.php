6e86b240ecea8584fb18b7df9275ccc4815fab36
kawax <kawaxbiz@gmail.com>
2017-10-29 03:50:22 +0900

    json-ld

--- app/Model/Presenter/ItemJsonTrait.php
+++ app/Model/Presenter/ItemJsonTrait.php
@@ -9,21 +9,33 @@ trait ItemJsonTrait
 {
     public function jsonLd()
     {
-        $context = cache()->remember('json_ld.' . $this->asin, 60 * 12, function () {
-            return Context::create(Product::class, [
-                'name'  => $this->title,
-                'brand' => data_get($this->item_attribute->attributes, 'Brand'),
-                'image' => $this->large_image,
-                'url'   => $this->detail_url,
 
-                'offers' => [
-                    'price'         => data_get($this->offers->offers, 'Offer.OfferListing.Price.Amount'),
-                    'priceCurrency' => data_get($this->offers->offers, 'Offer.OfferListing.Price.CurrencyCode'),
-                    'itemCondition' => data_get($this->offers->offers, 'Offer.OfferListing.OfferAttributes.Condition'),
-                    'availability'  => data_get($this->offers->offers, 'Offer.OfferListing.Availability'),
-                ],
-            ]);
-        });
+        $ava = data_get($this->offers->offers, 'Offer.OfferListing.Availability');
+        if (str_contains($ava, '在庫あり')) {
+            $availability = 'http://schema.org/InStock';
+        } else {
+            $availability = null;
+        }
+
+        $context = Context::create(Product::class, [
+            'name'        => $this->title,
+            'brand'       => data_get($this->item_attribute->attributes, 'Brand'),
+            'sku' => data_get($this->item_attribute->attributes, 'SKU'),
+
+            'image' => $this->large_image,
+            'url'   => $this->detail_url,
+            'mpn' => data_get($this->item_attribute->attributes, 'MPN'),
+
+            'category'    => data_get($this->item_attribute->attributes, 'Binding'),
+            'model' => data_get($this->item_attribute->attributes, 'Model'),
+
+            'offers' => [
+                'price'         => data_get($this->offers->offers, 'Offer.OfferListing.Price.Amount'),
+                'priceCurrency' => data_get($this->offers->offers, 'Offer.OfferListing.Price.CurrencyCode'),
+                'itemCondition' => data_get($this->offers->offers, 'Offer.OfferListing.OfferAttributes.Condition'),
+                'availability'  => $availability,
+            ],
+        ]);
 
         return $context;
     }
