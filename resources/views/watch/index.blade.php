80b17296609ba41863ca8478c20a65983917c720
kawax <kawaxbiz@gmail.com>
2017-11-03 11:27:53 +0900

    csv

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -9,10 +9,13 @@
 
   <h1 class="uk-heading-divider">ウォッチリスト</h1>
 
+  @feature('csv_download')
   <div class="uk-alert-primary" uk-alert>
-    <p>CSVには当サイト内で保存済みのデータのみ含まれます。{{ auth()->user()->csvLimit() }}件分まで。件数指定のダウンロードは<a
-        href="{{ route('export.index') }}">エクスポート</a>から。</p>
+    <p>CSVには当サイト内で保存済みのデータのみ含まれます。{{ auth()->user()->csvLimit() }}件分まで。件数指定のダウンロードは
+      <a href="{{ route('export.index') }}">エクスポート</a>から。</p>
   </div>
+  @endfeature
+
 
   <div class="uk-grid-divider" uk-grid>
     <div class="uk-width-1-2@m">
