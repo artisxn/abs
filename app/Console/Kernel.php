fa923c005cbcad3a78f76258c978297b412ef978
kawax <kawaxbiz@gmail.com>
2018-02-26 21:52:14 +0900

    onOneServer()

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,71 +26,86 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('horizon:snapshot')
-                 ->everyFiveMinutes();
+                 ->everyFiveMinutes()
+                 ->onOneServer();
 
 
         //負荷分散のため時間は多少ずらす
 
         $schedule->command('abs:random-browse')
                  ->hourlyAt(21)
-                 ->when(config('feature.random_browse'));
+                 ->when(config('feature.random_browse'))
+                 ->onOneServer();
 
         $schedule->command('abs:recent-item')
                  ->hourlyAt(33)
-                 ->when(config('feature.recent_item'));
+                 ->when(config('feature.recent_item'))
+                 ->onOneServer();
 
         $schedule->command('abs:watch-item')
                  ->dailyAt('00:15')
-                 ->when(config('feature.watch_item'));
+                 ->when(config('feature.watch_item'))
+                 ->onOneServer();
 
         $schedule->command('abs:count-info')
-                 ->dailyAt('12:00');
+                 ->dailyAt('12:00')
+                 ->onOneServer();
 
         $schedule->command(Commands\Mainte\UpdateOldItem::class)
                  ->hourlyAt(25)
-                 ->when(config('feature.update_old_item'));
+                 ->when(config('feature.update_old_item'))
+                 ->onOneServer();
 
         $schedule->command(Commands\Mainte\DeleteOldItem::class)
                  ->daily()
-                 ->when(config('feature.delete_old_item'));
+                 ->when(config('feature.delete_old_item'))
+                 ->onOneServer();
 
         $schedule->command(Commands\Mainte\DeleteOldPost::class)
                  ->dailyAt('06:06')
-                 ->when(config('feature.delete_old_post'));
+                 ->when(config('feature.delete_old_post'))
+                 ->onOneServer();
 
         $schedule->command('abs:delete-category')
                  ->hourlyAt(44)
-                 ->when(config('feature.delete_category'));
+                 ->when(config('feature.delete_category'))
+                 ->onOneServer();
 
         $schedule->command('abs:world-watch')
                  ->everyFiveMinutes()
-                 ->when(config('feature.world_watch_item'));
+                 ->when(config('feature.world_watch_item'))
+                 ->onOneServer();
 
         //優先度の高いものは多く更新
         $schedule->command('abs:world-watch --priority=1')
                  ->everyTenMinutes()
-                 ->when(config('feature.world_watch_item'));
+                 ->when(config('feature.world_watch_item'))
+                 ->onOneServer();
 
         $schedule->command(Commands\PriceAlertCommand::class)
                  ->hourly()
-                 ->when(config('feature.price_alert'));
+                 ->when(config('feature.price_alert'))
+                 ->onOneServer();
 
         $schedule->command(Commands\WatchPriceAlertCommand::class)
                  ->hourly()
-                 ->when(config('feature.price_alert'));
-
+                 ->when(config('feature.price_alert'))
+                 ->onOneServer();
 
         $schedule->command('abs:watch-item')
                  ->hourlyAt(45)
-                 ->when(config('feature.price_alert_express'));
+                 ->when(config('feature.price_alert_express'))
+                 ->onOneServer();
 
         $schedule->command(Commands\WatchPriceAlertCommand::class)
                  ->everyThirtyMinutes()
-                 ->when(config('feature.price_alert_express'));
+                 ->when(config('feature.price_alert_express'))
+                 ->onOneServer();
 
         $schedule->command(Commands\World\ChatWorkCommand::class)
                  ->dailyAt('09:00')
-                 ->when(config('feature.chatwork'));
+                 ->when(config('feature.chatwork'))
+                 ->onOneServer();
     }
 
     /**
