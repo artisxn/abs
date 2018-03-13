838554b6cabc8ffc8068f934440555ead5c90d6f
kawax <kawaxbiz@gmail.com>
2018-03-13 13:02:51 +0900

    rename

--- database/migrations/2017_11_04_000000_alter_post_nullable_fields_table.php
+++ database/migrations/2017_11_04_000000_alter_post_nullable_fields_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterPostNullableFieldsTablePost extends Migration
+class AlterPostNullableFieldsTable extends Migration
 {
     /**
      * Run the migrations.
