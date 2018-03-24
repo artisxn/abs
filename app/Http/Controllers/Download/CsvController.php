f43a5ae5c7789fb22aead9d87b68e48d7d120ee7
kawax <kawaxbiz@gmail.com>
2018-03-24 14:12:58 +0900

    export

--- app/Http/Controllers/Download/CsvController.php
+++ app/Http/Controllers/Download/CsvController.php
@@ -13,7 +13,7 @@ class CsvController extends Controller
      * @param Request $request
      * @param         $file_name
      *
-     * @return \Illuminate\Http\Response
+     * @return \Symfony\Component\HttpFoundation\StreamedResponse
      */
     public function __invoke(Request $request, $file_name)
     {
@@ -21,9 +21,11 @@ class CsvController extends Controller
 
         abort_unless(Storage::exists($name), 404);
 
-        $file = storage_path('app/' . $name);
+        //        $file = Storage('app/' . $name);
 
-        return response()->download($file, $file_name)
-                         ->deleteFileAfterSend(true);
+        return Storage::download($name);
+
+        //        return response()->download($file, $file_name)
+        //                         ->deleteFileAfterSend(true);
     }
 }
