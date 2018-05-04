f185dcc03bf6395cc9eb2a6878edcc05ed7ecb6f
kawax <kawaxbiz@gmail.com>
2018-05-04 19:34:06 +0900

    sw

--- public/sw.js
+++ public/sw.js
@@ -4,8 +4,10 @@
   const WebPush = {
     init () {
       self.addEventListener('push', this.notificationPush.bind(this))
-      self.addEventListener('notificationclick', this.notificationClick.bind(this))
-      self.addEventListener('notificationclose', this.notificationClose.bind(this))
+      self.addEventListener('notificationclick',
+        this.notificationClick.bind(this))
+      self.addEventListener('notificationclose',
+        this.notificationClose.bind(this))
     },
 
     /**
@@ -23,16 +25,17 @@
       // Check if the notification has a payload.
       if (event.data) {
         event.waitUntil(
-            this.sendNotification(event.data.json())
+          this.sendNotification(event.data.json()),
         )
-      } else {
-      // Otherwise just fetch the last notification from the server.
+      }
+      else {
+        // Otherwise just fetch the last notification from the server.
         event.waitUntil(
           self.registration.pushManager.getSubscription().then(subscription => {
             if (subscription) {
               return this.fetchNofication(subscription)
             }
-          })
+          }),
         )
       }
     },
@@ -49,9 +52,11 @@
 
       if (event.action === 'open') {
         self.clients.openWindow('/')
-      } else if (event.action === 'other') {
+      }
+      else if (event.action === 'other') {
         //
-      } else {
+      }
+      else {
         self.clients.openWindow('/')
       }
     },
@@ -81,7 +86,7 @@
         body: data.body,
         icon: data.icon || '/icon.png',
         data: data,
-        actions: data.actions || []
+        actions: data.actions || [],
       })
     },
 
@@ -91,16 +96,18 @@
      * @param  {String} subscription.endpoint
      * @return {Response}
      */
-    fetchNofication ({ endpoint }) {
-      return fetch(`/push/notifications/last?endpoint=${encodeURIComponent(endpoint)}`).then(response => {
-        if (response.status !== 200) {
-          throw new Error()
-        }
+    fetchNofication ({endpoint}) {
+      return fetch(
+        `/push/notifications/last?endpoint=${encodeURIComponent(endpoint)}`).
+        then(response => {
+          if (response.status !== 200) {
+            throw new Error()
+          }
 
-        return response.json().then(data => {
-          return this.sendNotification(data)
+          return response.json().then(data => {
+            return this.sendNotification(data)
+          })
         })
-      })
     },
 
     /**
@@ -110,7 +117,7 @@
      * @param  {String} subscription.endpoint
      * @return {Response}
      */
-    dismissNotification ({ notification }, { endpoint }) {
+    dismissNotification ({notification}, {endpoint}) {
       if (!notification.data.id) {
         return
       }
@@ -121,10 +128,14 @@
       // Send a request to the server to mark the notification as read.
       fetch(`/push/notifications/${notification.data.id}/dismiss`, {
         method: 'POST',
-        body: data
+        body: data,
       })
-    }
+    },
   }
 
   WebPush.init()
+})()
+
+(() => {
+  self.addEventListener('fetch', function (event) {})
 })()
