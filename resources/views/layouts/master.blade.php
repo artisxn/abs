edc57b837a209bb3c37e2ddb1c6a2b3abcea411f
kawax <kawaxbiz@gmail.com>
2018-05-04 19:29:45 +0900

    pwa

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,41 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-  @include('layouts.analytics')
+    @include('layouts.analytics')
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <meta name="vapidPublicKey" content="{{ config('webpush.vapid.public_key') }}">
+    <meta name="vapidPublicKey" content="{{ config('webpush.vapid.public_key') }}">
 
-  <title>@yield('title'){{ config('app.name') }}</title>
+    <title>@yield('title'){{ config('app.name') }}</title>
 
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-  <link rel='icon' type='image/png' href='/icon.png'>
-  <link rel="apple-touch-icon" sizes="144x144" href="/icon.png">
+    <link rel='icon' type='image/png' href='/icon.png'>
+    <link rel="apple-touch-icon" sizes="144x144" href="/icon.png">
+    <link rel="apple-touch-icon" sizes="192x192" href="/icon-192x192.png">
 
-  @include('layouts.analytics_verification')
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#1e87f0"/>
 
-  @include('layouts.preload')
+    @include('layouts.analytics_verification')
 
-  <meta name="author" content="kawax">
+    @include('layouts.preload')
+
+    <meta name="author" content="kawax">
 </head>
 <body>
 <div id="app" class="uk-offcanvas-content">
 
-  @include('layouts.header')
+    @include('layouts.header')
 
-  <main class="uk-container">
+    <main class="uk-container">
 
-    @yield('content')
+        @yield('content')
 
-  </main>
+    </main>
 
-  @include('layouts.footer')
+    @include('layouts.footer')
 
 </div>
 
