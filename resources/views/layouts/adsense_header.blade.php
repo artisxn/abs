762063ea39d3f738b9057cde87db6f7250bb9084
kawax <kawaxbiz@gmail.com>
2018-05-26 23:02:36 +0900

    ads

--- resources/views/layouts/adsense_header.blade.php
+++ resources/views/layouts/adsense_header.blade.php
@@ -1,7 +1,11 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-5342783238467976",
-    enable_page_level_ads: true
-  });
-</script>
+@isset($asin_item)
+    @unless(data_get($asin_item->item_attribute->attributes,'IsAdultProduct'))
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: 'ca-pub-5342783238467976',
+            enable_page_level_ads: true,
+          })
+        </script>
+    @endunless
+@endisset
