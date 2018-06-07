45f92a5f883f1f5691424ebd7393043368066310
kawax <kawaxbiz@gmail.com>
2018-06-08 07:07:37 +0900

    adsense

--- resources/views/layouts/adsense.blade.php
+++ resources/views/layouts/adsense.blade.php
@@ -1,12 +1,10 @@
-@unless(data_get($asin_item->item_attribute->attributes,'IsAdultProduct'))
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- abs -->
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-client="ca-pub-5342783238467976"
-         data-ad-slot="2645629896"
-         data-ad-format="auto"></ins>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({})
-    </script>
-@endunless
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- abs -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5342783238467976"
+     data-ad-slot="2645629896"
+     data-ad-format="auto"></ins>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({})
+</script>
