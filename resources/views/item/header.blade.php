345b80cedd0bb45c175a749c95d91e3a6eef6ede
kawax <kawaxbiz@gmail.com>
2018-06-10 19:56:38 +0900

    cu

--- resources/views/item/header.blade.php
+++ resources/views/item/header.blade.php
@@ -1,7 +1,7 @@
 
 <div class="uk-card-header uk-card-primary">
   <h2 class="uk-card-title">
-    <a href="{{ route('asin', data_get($item, 'ASIN')) }}">
+    <a href="{{ route('asin', data_get($item, 'ASIN', '')) }}">
       <span itemprop="name">{{ abs_decode(data_get($item, 'ItemAttributes.Title')) }}</span>
     </a>
   </h2>
