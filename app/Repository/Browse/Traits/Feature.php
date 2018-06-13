74d0938aaea4b2e6772dab9822ca38b0e476118d
kawax <kawaxbiz@gmail.com>
2018-06-13 16:12:32 +0900

    feature

--- app/Repository/Browse/Traits/Feature.php
+++ app/Repository/Browse/Traits/Feature.php
@@ -38,11 +38,12 @@ trait Feature
             return $this->browse->find($browse)
                                 ->items()
                                 ->select(['asin', 'title', 'rank', 'availability_id'])
+                                ->where('availability_id', 13)
                                 ->whereNotNull('rank')
                                 ->with(['availability'])
-                                ->whereHas('availability', function ($query) {
-                                    $query->where('availability', '近日発売　予約可');
-                                })
+                //                                ->whereHas('availability', function ($query) {
+                //                                    $query->where('availability', '近日発売　予約可');
+                //                                })
                                 ->orderBy('rank')
                                 ->limit(100)
                                 ->get();
