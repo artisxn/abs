afad74233ddab138907608c757206a7876b3d94e
kawax <kawaxbiz@gmail.com>
2018-06-14 00:32:28 +0900

    DeleteOldHistory

---
+++ app/Console/Commands/Mainte/DeleteOldHistory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands\Mainte;
+
+use Illuminate\Console\Command;
+
+use App\Model\History;
+
+class DeleteOldHistory extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'abs:delete-old-history';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '古いHistoryを削除';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $items = History::whereDate('updated_at', '<', now()->subDays(200));
+
+        info('Delete Old History: ' . $items->count());
+
+        $items->delete();
+    }
+}
