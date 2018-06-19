cce700b8df7062a3fcd1349cfca31481d40c98a4
kawax <kawaxbiz@gmail.com>
2018-06-20 01:49:38 +0900

    feature form

--- resources/views/feature/info.blade.php
+++ resources/views/feature/info.blade.php
@@ -1,3 +1,6 @@
 <div class="uk-alert-primary uk-margin-top" uk-alert>
-    <p>「特定のカテゴリーのこういう情報が欲しい」という依頼が多いのでサンプルとしていくつか作る。</p>
+    <p>
+        「特定のカテゴリーのこういう情報が欲しい」という依頼が多いのでサンプルとしていくつか作る。
+        <a href="https://goo.gl/forms/30quAHme649p6ktX2">フォーム</a>で受付中。
+    </p>
 </div>
