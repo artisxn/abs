947fde4d0ef4223a79aac576af395da6dbd49f45
kawax <kawaxbiz@gmail.com>
2018-07-02 12:26:31 +0900

    db

--- config/database.php
+++ config/database.php
@@ -47,13 +47,13 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            //            'read'      => [
-            //                'host' => env('DB_HOST_READ', env('DB_HOST', 'localhost')),
-            //            ],
-            //            'write'     => [
-            //                'host' => env('DB_HOST', 'localhost'),
-            //            ],
+            //            'host'      => env('DB_HOST', 'localhost'),
+            'read'      => [
+                'host' => env('DB_HOST_READ', env('DB_HOST', 'localhost')),
+            ],
+            'write'     => [
+                'host' => env('DB_HOST', 'localhost'),
+            ],
             //            'sticky'    => true,
             'port'      => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'forge'),
