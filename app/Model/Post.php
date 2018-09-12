14d4d7565b9fcffd555c5d518a13d770ba881ed6
kawax <kawaxbiz@gmail.com>
2018-09-12 14:49:29 +0900

    post

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -3,12 +3,15 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 use Illuminate\Notifications\Notifiable;
 
 class Post extends Model
 {
     use Notifiable;
+
+    const PUBLISHED = 'PUBLISHED';
 
     /**
      *
@@ -19,5 +22,17 @@ class Post extends Model
     public function scopePickup($query)
     {
         return $query->whereCategoryId(1)->published()->latest()->limit(3);
+    }
+
+    /**
+     * Scope a query to only published scopes.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopePublished(Builder $query)
+    {
+        return $query->where('status', '=', static::PUBLISHED);
     }
 }
